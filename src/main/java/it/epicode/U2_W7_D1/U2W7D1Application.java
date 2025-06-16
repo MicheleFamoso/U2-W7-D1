a04231f3be6f22c771d873a0fc42c820f273cc5a
MicheleFamoso <michelefamoso@gmail.com>
2025-06-16 16:02:13 +0200

    Initial commit

---
+++ src/main/java/it/epicode/U2_W7_D1/U2W7D1Application.java
@@ -0,0 +1,13 @@
+package it.epicode.U2_W7_D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W7D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W7D1Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/U2_W7_D1/U2W7D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.U2_W7_D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W7D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
